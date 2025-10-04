d0542a05c3e48741bdbd5b5e17a25d71b1748490
Rudivanlf <rudivanbsns@gmail.com>
2025-10-04 19:47:27 -0300

    Alteracao em SecurityConfig para a rodar a documentacao com swagger

--- src/main/java/com/project/cryptography/Config/SecurityConfig.java
+++ src/main/java/com/project/cryptography/Config/SecurityConfig.java
@@ -1,4 +1,4 @@
-package com.project.cryptography.config;
+package com.project.cryptography.Config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,6 +13,13 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 public class SecurityConfig {
 
+    private static final String[] SWAGGER_WHITELIST = {
+            "/swagger-ui/**",
+            "/v3/api-docs/**",
+            "/swagger-resources/**",
+            "/swagger-resources"
+    };
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
@@ -20,6 +27,7 @@ public class SecurityConfig {
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)) // Não cria sessão
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers("/users/**").permitAll() // Permite acesso a todos os endpoints dos users
+                        .requestMatchers(SWAGGER_WHITELIST).permitAll()
                         .anyRequest().authenticated() // Exige autenticação para qualquer outra requisição
                 );
         return http.build();
